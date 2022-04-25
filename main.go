2e0c326464b918b9d1fd1274bb1b4f254ebe8c9b
Dawid Jach <jach.developer@protonmail.com>
2022-04-25 20:43:25 +0200

    Add server error handler

--- main.go
+++ main.go
@@ -43,4 +43,9 @@ func main() {
 	// Start up message
 	fmt.Printf("Starting server at port 8080\n")
 
+	// Handle errors
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
